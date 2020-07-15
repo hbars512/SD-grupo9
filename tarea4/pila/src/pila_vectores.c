401e052a629a713f460dcea5181a1a559f3fca0c
Herbert Arias <dayanqwe123@gmail.com>
2020-07-15 14:55:06 -0500

    fix: corrigiendo errores de integracion

--- tarea4/pila/src/pila_vectores.c
+++ tarea4/pila/src/pila_vectores.c
@@ -26,6 +26,7 @@ int empila(int dato);
 int pilaVacia();
 int desempila();
 int topo();
+void mostrar();
 
 
 
@@ -38,9 +39,8 @@ int main() {
 			printf("\n 1: Crear la pila.");
 			printf("\n 2: Mostrar la pila.");
 			printf("\n 3: Insertar elemento.");
-			printf("\n 4: Salir.");
-			printf("\n 5: Desempila");
-			printf("\n 6: topo");
+			printf("\n 4: Desempila");
+			printf("\n 5: Salir");
 			printf("\n\n\tOpcion: ");
 			scanf("%d", &option);
 			switch(option) {
@@ -48,24 +48,29 @@ int main() {
 						printf("\n\tLa pila ah sido creada.");
 						break;
 				case 2:
+						mostrar();
 						break;
 				case 3:
 						printf("Ingresa un numero: ");
 						scanf("%i", &dat);
 						numero = empila(dat);
-						printf("\n\tSe ha insertado el elemento %i exitosamente.", numero);
+						printf("\n\tSe ha insertado el elemento %i exitosamente.", aPila.dato[numero]);
 						break;
 				case 4:
+						numero = desempila();
+						if(numero == ERRORPILAVACIA) {
+							printf("\n\tLa pila está vacía.");
+						} else {
+							printf("\n\tSe ha desempilado el elemento %i exitosamente.", aPila.dato[numero]);
+						}
 						break;
 				case 5:
-						desempila();
-						printf("desempila");
-				case 6:
-						topo();
-						printf("topo");
+						printf("\n\tPrograma terminado");
+						return 0;
+						break;
 
 			}
-		} while(option != 6);
+		} while(option != 5);
 	return 0;
 }
 void inicializarPila() {
@@ -73,20 +78,22 @@ void inicializarPila() {
 }
 
 int pilaLlena() {
-	return aPila.topo == MAXPILA - 1? 1:0;
+	if (aPila.topo == MAXPILA - 1) {
+		return 1;
+	}
+	return 0;
 }
 
 int pilaVacia() {
 	if (aPila.topo == -1) {
 		return 1;
-	} else {
-		return 0;
 	}
+	return 0;
 }
 
 int empila(int d)
 {
-	if (pilaLlena(aPila)) {
+	if (pilaLlena()) {
 		return ERRORPILALLENA;
 	} else {
 		aPila.topo = aPila.topo + 1;
@@ -97,24 +104,25 @@ int empila(int d)
 
 int desempila()
 {
-	if(pilaVacia){
+	if(pilaVacia()){
 		return ERRORPILAVACIA;
-		} else{
-			aPila.topo = aPila.topo-1;
-			return aPila.topo;
 		}
-
+	aPila.topo = aPila.topo-1;
+	return aPila.topo;
 }
 
 int topo()
 {
-	if (pilaVacia){
+	if (pilaVacia()){
 		printf("error pila vacia al acceder ");
 		return ERRORPILAVACIA;
-
-	} else{
-		return aPila.dato[aPila.topo];
 	}
+	return aPila.dato[aPila.topo];
 }
 
-
+void mostrar() {
+	int i;
+	for(i = 0; i < aPila.topo + 1; i++) {
+		printf("%d	", aPila.dato[i]);
+	}
+}
